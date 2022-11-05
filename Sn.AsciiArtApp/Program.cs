3e72e7bb24c06a9664fb81d3d205cddf9e25232a
SlimeNull <slimenull@outlook.com>
2022-11-06 00:00:16 +0800

    同时支持文件与文件夹

--- Sn.AsciiArtApp/Program.cs
+++ Sn.AsciiArtApp/Program.cs
@@ -4,7 +4,7 @@ using System.Diagnostics;
 
 if (args.Length == 0)
 {
-    Console.WriteLine("Usage: TestConsole.exe <image files folder> ...");
+    Console.WriteLine("Usage: Sn.AsciiArtApp <path> ...");
     return;
 }
 
@@ -12,36 +12,57 @@ Console.WriteLine("Processing...");
 
 AsciiSkin[] skins = AsciiSkin.Create(AsciiSkin.DefaultCharactors);
 
-foreach (string folder in args)
+foreach (string path in args)
 {
-    DirectoryInfo info = new DirectoryInfo(folder);
-    if (!info.Exists)
+    if (Directory.Exists(path))
     {
-        Console.WriteLine($"{folder} is not exist");
-        continue;
-    }
-
-    Console.WriteLine($"Folder: {folder}");
-    foreach (FileInfo file in info.GetFiles())
-    {
-        if (!string.Equals(file.Extension, ".JPG", StringComparison.OrdinalIgnoreCase) &&
-            !string.Equals(file.Extension, ".PNG", StringComparison.OrdinalIgnoreCase) &&
-            !string.Equals(file.Extension, ".BMP", StringComparison.OrdinalIgnoreCase))
+        DirectoryInfo info = new DirectoryInfo(path);
+        Console.WriteLine($"Folder: {path}");
+        foreach (FileInfo file in info.GetFiles())
         {
+            if (!string.Equals(file.Extension, ".JPG", StringComparison.OrdinalIgnoreCase) &&
+                !string.Equals(file.Extension, ".PNG", StringComparison.OrdinalIgnoreCase) &&
+                !string.Equals(file.Extension, ".BMP", StringComparison.OrdinalIgnoreCase))
+            {
+                continue;
+            }
+
+            try
+            {
+                Console.WriteLine($"  Image {file.Name} ...");
+                using SKBitmap bmp = SKBitmap.Decode(file.FullName);
+                string result = AsciiArtGen.Generate(bmp, skins);
+                string output = Path.ChangeExtension(file.FullName, ".txt");
+                File.WriteAllText(output, result);
+            }
+            catch
+            {
+                Console.WriteLine($"  Failed {file.Name}.");
+            }
+        }
+    }
+    else if (File.Exists(path))
+    {
+        FileInfo info = new FileInfo(path);
+        if (!string.Equals(info.Extension, ".JPG", StringComparison.OrdinalIgnoreCase) &&
+            !string.Equals(info.Extension, ".PNG", StringComparison.OrdinalIgnoreCase) &&
+            !string.Equals(info.Extension, ".BMP", StringComparison.OrdinalIgnoreCase))
+        {
+            Console.WriteLine($"  File {info.Name} is not an image file.");
             continue;
         }
 
         try
         {
-            Console.WriteLine($"  Image {file.Name} ...");
-            using SKBitmap bmp = SKBitmap.Decode(file.FullName);
+            Console.WriteLine($"  Image {info.Name} ...");
+            using SKBitmap bmp = SKBitmap.Decode(info.FullName);
             string result = AsciiArtGen.Generate(bmp, skins);
-            string output = Path.ChangeExtension(file.FullName, ".txt");
+            string output = Path.ChangeExtension(info.FullName, ".txt");
             File.WriteAllText(output, result);
         }
         catch
         {
-            Console.WriteLine($"  Failed {file.Name}.");
+            Console.WriteLine($"  Failed {info.Name}.");
         }
     }
 }
